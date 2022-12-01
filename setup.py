7456c80cce7c7de92bc01802d01bf5e033efbb84
JorgeLuizFranco <63986301+JorgeLuizFranco@users.noreply.github.com>
2022-11-30 23:35:51 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='mdllib',
+    version='0.1.0',
+    description='',
+    url='',
+    author='Jorge Luiz Franco',
+    author_email='jorge.luiz@usp.br',
+    license='BSD 2-clause',
+    packages=[
+        'mdllib'
+    ],
+    install_requires=[
+        'numpy', 'pandas'
+    ],
+    classifiers=[],
+)
